d9caf45623e0135183d7f58a164a08768acba89c
Simon Maurer <simon.maurer@zeiss.com>
2022-02-18 11:21:57 +0100

    Improved output to highlight issue

--- StaticLibWithStaticVariable/StaticVariable.cpp
+++ StaticLibWithStaticVariable/StaticVariable.cpp
@@ -1,8 +1,8 @@
 #include "StaticVariable.h"
 #include <iostream>
-static int myStaticVariable = 0;
+static int myStaticVariable = 0; //This might be a singleton instance or similar
 
 void printAndIncrementStaticVariable()
 {
-  std::cout << myStaticVariable++ << "\n";
+  std::cout << __FILE__ << ":L" << __LINE__ << ": Value of static variable changed from " << myStaticVariable++ << " to " << myStaticVariable << "\n";
 }
